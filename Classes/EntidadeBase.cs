029df1edd5decd1788bd690e574cef25d1d992db
Wendell Barbieri <wendell.t.barbieri@gmail.com>
2022-07-29 15:25:44 -0300

    wenflix

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace wenflix.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get; protected set;}
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,17 @@
+namespace wenflix
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Musical = 9,
+        Romance = 10,
+        
+    }
+}
